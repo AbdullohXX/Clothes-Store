52ca0e5304e12a440e91a7ab8e3a769c1882b6db
AbdullohXX <abdullohxojimurodov1@gmail.com>
2022-09-30 21:41:50 +0500

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+const a = document.getElementById("facebook");
+      const b = document.getElementById("youtube");
+      const c = document.getElementById("google");
+      const d = document.getElementById("twitter");
+      const e = document.getElementById("linked");
+      const f = document.getElementById("instagram");
+
+      a.onclick = function(){
+        window .open("https://www.facebook.com/abdulloh.xojimurodov");
+      }
+      b.onclick = function(){
+        window .open("https://www.youtube.com/playlist?list=PLJ_EMtihYJo72dzoT_17hCY_auHzJ13PF");
+      }
+      c.onclick = function(){
+        window .open("https://moylov.netlify.app/");
+      }
+      d.onclick = function(){
+        window .open("https://twitter.com/Abdullo83807307");
+      }
+      e.onclick = function(){
+        window .open("https://www.youtube.com/playlist?list=PLJ_EMtihYJo72dzoT_17hCY_auHzJ13PF");
+      }
+      f.onclick = function(){
+        window .open("https://www.instagram.com/xojimurodov545/");
+      }
